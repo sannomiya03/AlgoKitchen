89611c4dbe621881f46a656b4c73cbc383db80d3
sannomiya03 <y.sannomiya03@gmail.com>
2017-01-14 17:44:59 +0900

    レーザーチャート初描画

--- src/components/recipesheet/recipesheet.vue
+++ src/components/recipesheet/recipesheet.vue
@@ -1,3 +1,5 @@
+<!-- template for the polygraph component. -->
+
 <template lang="pug">
 	section.recipesheet
 		section.editer-section
@@ -12,7 +14,7 @@
 					span 人前
 		section.editer-section
 			h2.editer-section-title 材料
-			section.input-item( v-for="food in recipesheet.foods" )
+			section.input-item( v-for="food,index in recipesheet.foods" )
 				p.input-item-title {{food.title}}
 				p.input-item-value
 					input( type="number" v-model="food.num")
@@ -20,6 +22,15 @@
 				p.input-item-value
 					input( type="number" v-model="food.price")
 					span 円/個
+				
+				svg( width="200" height="200" )
+					g
+						polygon( :points="points(food.nutrients)")
+						circle( cx="100" cy="100" r="80" )
+						axis-label( v-for="(nutrient, index) in food.nutrients",
+							:stat="nutrient",
+							:index="index",
+							:total="food.nutrients.length")
 		section.editer-section
 			h2.editer-section-title 作業工程
 			section.input-item( v-for="step in recipesheet.steps" )
@@ -37,7 +48,25 @@
 
 <script lang="coffee">
 	module.exports =
+		mounted: ->
+			return
 		methods:
+			points: (nutrients)->
+				total = nutrients.length
+				points = ""
+				for nutrient, index in nutrients
+					value = nutrient.value
+					x = 0
+					y = -value * 0.8
+					angle = Math.PI * 2 / total * index
+					cos = Math.cos(angle)
+					sin = Math.sin(angle)
+					tx = x * cos - y * sin + 100
+					ty = x * sin + y * cos + 100
+					points += tx+","+ty
+					if index < nutrients.length - 1 then points += ","
+				return points
+
 			calcSumTime: (steps, sumTime=0) ->
 				for step in steps then sumTime+=step.time
 				return sumTime
@@ -51,15 +80,15 @@
 					fiber: 0
 					fat: 0
 					carb: 0
-				for food in foods
-					totalNutrients.calorie += food.nutrients.calorie
-					totalNutrients.ash += food.nutrients.ash
-					totalNutrients.cholesterol += food.nutrients.cholesterol
-					totalNutrients.protein += food.nutrients.protein
-					totalNutrients.water += food.nutrients.water
-					totalNutrients.fiber += food.nutrients.fiber
-					totalNutrients.fat += food.nutrients.fat
-					totalNutrients.carb += food.nutrients.carb
+				# for food in foods
+				# 	totalNutrients.calorie += food.nutrients.calorie
+				# 	totalNutrients.ash += food.nutrients.ash
+				# 	totalNutrients.cholesterol += food.nutrients.cholesterol
+				# 	totalNutrients.protein += food.nutrients.protein
+				# 	totalNutrients.water += food.nutrients.water
+				# 	totalNutrients.fiber += food.nutrients.fiber
+				# 	totalNutrients.fat += food.nutrients.fat
+				# 	totalNutrients.carb += food.nutrients.carb
 				return totalNutrients
 			calcSumPrice: (foods, sumFoods=0) ->
 				for food in foods then sumFoods += food.num * food.price
@@ -75,57 +104,61 @@
 					foods: [
 						{
 							title: "じゃがいも"
-							nutrients:
-								calorie: 18
-								ash: 0.8
-								cholesterol: 0
-								protein: 2.2
-								water: 93.2
-								fiber: 2.4
-								fat: 0.1
-								carb: 3.4
+							nutrients: [
+								{ label: "calorie", value: 100 }
+								{ label: "ash", value: 90 }
+								{ label: "cholesterol", value: 80 }
+								{ label: "protein", value: 70 }
+								{ label: "water", value: 60 }
+								{ label: "fiber", value: 50 }
+								{ label: "fat", value: 40 }
+								{ label: "carb", value: 10 }
+							]
 							num: 1
 							price: 100
 						}
 						{
 							title: "にんじん"
-							nutrients:
-								calorie: 18
-								ash: 0.8
-								cholesterol: 0
-								protein: 2.2
-								water: 93.2
-								fiber: 2.4
-								fat: 0.1
-								carb: 3.4
+							nutrients: [
+								{ label: "calorie", value: 100 }
+								{ label: "ash", value: 100 }
+								{ label: "cholesterol", value: 100 }
+								{ label: "protein", value: 100 }
+								{ label: "water", value: 100 }
+								{ label: "fiber", value: 100 }
+								{ label: "fat", value: 100 }
+								{ label: "carb", value: 100 }
+							]
 							num: 1
 							price: 100
 						}
 						{
 							title: "たまねぎ"
-							nutrients:
-								calorie: 18
-								ash: 0.8
-								cholesterol: 0
-								protein: 2.2
-								water: 93.2
-								fiber: 2.4
-								fat: 0.1
-								carb: 3.4
+							nutrients: [
+								{ label: "calorie", value: 100 }
+								{ label: "ash", value: 100 }
+								{ label: "cholesterol", value: 100 }
+								{ label: "protein", value: 100 }
+								{ label: "water", value: 100 }
+								{ label: "fiber", value: 100 }
+								{ label: "fat", value: 100 }
+								{ label: "carb", value: 100 }
+							]
 							num: 1
 							price: 100
 						}
 						{
 							title: "カレールウ"
-							nutrients:
-								calorie: 18
-								ash: 0.8
-								cholesterol: 0
-								protein: 2.2
-								water: 93.2
-								fiber: 2.4
-								fat: 0.1
-								carb: 3.4
+							nutrients: [
+								{ label: "calorie", value: 100 }
+								{ label: "ash", value: 100 }
+								{ label: "cholesterol", value: 100 }
+								{ label: "protein", value: 100 }
+								{ label: "water", value: 100 }
+								{ label: "fiber", value: 100 }
+								{ label: "fat", value: 100 }
+								{ label: "carb", value: 100 }
+							]
 							num: 1
 							price: 100
 						}
@@ -149,18 +182,6 @@
 							time: 15
 						}
 					]
-					result:
-						times: 0
-						nutrients:
-							calorie: 0
-							ash: 0
-							cholesterol: 0
-							protein: 0
-							water: 0
-							fiber: 0
-							fat: 0
-							carb: 0
-						price: 0
 			}
 </script>
 
@@ -185,4 +206,19 @@
 		color: $Gray700
 		// border-top: 2px solid $Gray700
 		margin-bottom: 0.5em
+
+	polygon
+		fill: #42b983
+		opacity: .75
+	circle
+		fill: transparent
+		stroke: #999
+	text
+		font-family: Helvetica Neue, Arial, sans-serif
+		font-size: 10px
+		fill: #666
+	label
+		display: inline-block
+		margin-left: 10px
+		width: 20px
 </style>
