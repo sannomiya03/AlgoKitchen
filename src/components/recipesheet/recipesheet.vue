1566ac49dea824a78129db476c3e61c5992f3037
sannomiya03 <y.sannomiya03@gmail.com>
2017-01-15 11:18:36 +0900

    Stepの表示にアイコンを追加

--- src/components/recipesheet/recipesheet.vue
+++ src/components/recipesheet/recipesheet.vue
@@ -26,16 +26,25 @@
 						p.input-item
 							span.label 単価：
 							input( type="number" v-model="food.price" min="0" v-bind:disabled="!food.use")
-							span.unit 円/個
-					td.diagram
-						rader( :width="getDiagramSize(food.use)", :height="getDiagramSize(food.use)", :arr="food.nutrients" )
+							span.unit 円/{{food.num}}個
+					//- td.diagram
+					//- 	rader( :width="getDiagramSize(food.use)", :height="getDiagramSize(food.use)", :arr="food.nutrients" )
 		section.editer-section
-			h2.editer-section-title 作業工程
-			section.input-item( v-for="step in recipesheet.steps" )
-				p.input-item-title {{step.title}}
-				p.input-item-value
-					input( type="number" v-model="step.time" min="0")
-					span 分
+			h2.editer-section-title 作り方
+			table.steps
+				tr.step( v-for="step,index in recipesheet.steps", :class="{ disabled: !step.use }" )
+					td.icon
+						input( type="checkbox", name="step.title", v-model="step.use" )
+						label( for="step.title", class="checkbox" )
+						div.img-container
+							img( src="http://localhost:8080/static/images/nife.svg" width="60px" height="60px")
+					td.info
+						h3.step-title() {{step.title}}
+					td.time
+						p.input-item
+							span.label 時間：
+							input( type="number" v-model="step.time" min="0")
+							span.unit 分
 		section.editer-section
 			h2.editer-section-title 合計
 			ul
@@ -88,33 +97,35 @@
 			border-top: 1px solid $Gray200
 			&:last-child
 				border-bottom: 1px solid $Gray200
-			td:nth-child(1)
+			td:first-child
 				width: 1.5em
 				position: relative
 				top: 0.6em
-			td:nth-child(4)
+			td:last-child
 				width: 80px
-			td:nth-child(3)
-				width: 130px
+			td.info
+				width: 140px
+		.unit
+			width: 5em
 		.food-title
 			+animate()
 			font-size: 12pt
 			cursor: pointer
-		.input-item
-			+animate()
-			opacity: 1
-			font-size: 8pt
-			color: $Gray700
-			margin-bottom: 2px
-			.label
-				width: 4em
-				text-align: right
-		input[type="number"]
-			font-size: 9pt
-			height: 2.2em
-			width: 2em
 		.diagram
 			text-align: center
+	.input-item
+		+animate()
+		opacity: 1
+		font-size: 8pt
+		color: $Gray700
+		margin-bottom: 1px
+		.label
+			width: 4em
+			text-align: right
+	input[type="number"]
+		font-size: 9pt
+		height: 2.2em
+		width: 2em
 	.food.disabled
 		.food-title
 			font-size: 8pt
@@ -124,4 +135,28 @@
 			font-size: 4pt
 		input[type="number"]
 			height: 0em
+	.steps
+		.step
+			border-top: 1px solid $Gray200
+			td:first-child
+				width: 80px
+			td:last-child
+				width: 120px
+			.icon
+				position: relative
+				width: 80px
+				height: 80px
+				input
+					position: relative
+					z-index: 9999
+				.img-container
+					position: absolute
+					top: 0
+					left: 0
+					width: 80px
+					height: 80px
+					padding: 15px
+					img
+						width: 100%
+						height: 100%
 </style>
